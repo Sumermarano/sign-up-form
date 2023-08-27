e13de7e97c8a13f99443604f2573d4cecd99e347
sumermarano <sumermarano@gmail.com>
2023-08-27 11:00:51 +0300

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const pass1 = document.getElementById("pass");
+  const pass2 = document.getElementById("conf-pass");
+  const passwordMatch = document.getElementById("password-match");
+
+  pass2.addEventListener("input", () => {
+    if (pass1.value !== pass2.value) {
+      passwordMatch.textContent = "* Passwords do not match";
+    } else {
+        passwordMatch.textContent = "";
+    }
+    if (pass2.value === "") {
+        passwordMatch.textContent = "";
+    }
+  });
+
+  pass1.addEventListener("input", () => {
+    if (pass1.value === "") {
+        passwordMatch.textContent = "";
+    }
+  })
+});
